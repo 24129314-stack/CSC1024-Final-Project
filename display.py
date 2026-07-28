afd50c9f0bb730e871d7ecf1a8f2d1b110abe41c
jaamunah <jaamunahrahj15@gmail.com>
2026-07-28 09:09:28 +0800

    display.py

---
+++ display.py
@@ -0,0 +1,65 @@
+def read_posts():
+
+    post_list = []
+
+    try:
+        with open("posts.txt", "r") as file:
+
+            for line in file:
+
+                line = line.strip()
+
+                if line != "":
+
+                    details = line.split("|")      
+                  
+                    part1 = details[0]
+                    part2 = details[1]
+                    part3 = details[2]
+                    part4 = details[3]
+                    part5 = details[4]
+                
+
+                    post = [
+                        part1,
+                        part2,
+                        part3,
+                        part4,
+                        part5
+                    ]
+
+                    post_list.append(post)
+
+    except FileNotFoundError:
+        print("posts.txt not found.")
+
+    return post_list
+
+
+
+
+def display_calendar(post_list):
+
+    if post_list [0]:
+        print("Not found.")
+        return
+
+    print("\n--------- CONTENT CALENDAR ---------")
+
+    print(f"{'Part1':<20}{'Part2':<20}{'Part3':<20}{'Part4':<20}{'Part5':<20}")
+
+    print("-" * 85)
+
+    for post in post_list:
+
+        print(
+            f"{post[0]:<20}"
+            f"{post[1]:<20}"
+            f"{post[2]:<20}"
+            f"{post[3]:<20}"
+            f"{post[4]:<20}"
+        )
+
+
+posts = read_posts()
+display_content_calendar(posts)
